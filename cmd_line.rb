9cb3bc6a2bdb438de543e52b1ef1bff949079f18
Erik Ebsen <erik.ebsen@me.com>
2013-07-29 21:34:55 -0500

    Added reference to ARGF I/O variable

--- cmd_line.rb
+++ cmd_line.rb
@@ -1,2 +1,4 @@
 puts "You gave #{ARGV.size} arguments."
 p ARGV
+
+# Often, the arguments to a program are the names of files that you want to process. In this case, you can use a second technique: the variable `ARGF` is a special kind of I/O object that acts like all the contents of all the files whose names are passed on the command line (or standard input if you don’t pass any filenames).
